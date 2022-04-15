286e08b5c660f453dc58eed01a19a0c5afb4d6e0
Alyona Gnevusheva <al.gnevusheva@gmail.com>
2022-04-15 18:34:11 +0400

    ïnitialized firebase

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,18 @@
+import { initializeApp } from 'firebase/app';
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAM1_X7HGd9skEZclFgitLNlSpq3IfR1xE',
+  authDomain: 'crwn-clothing-db-7bd27.firebaseapp.com',
+  projectId: 'crwn-clothing-db-7bd27',
+  storageBucket: 'crwn-clothing-db-7bd27.appspot.com',
+  messagingSenderId: '525859999027',
+  appId: '1:525859999027:web:9716c6fc1e1d111f2f4246',
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
