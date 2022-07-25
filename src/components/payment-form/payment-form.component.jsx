9b4447ed54e9c1631c2278831e91c14670c3cc5a
Alena <a.gnevusheva@gmail.com>
2022-07-25 21:15:27 +0400

    payment handler

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -1,6 +1,10 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js"
-import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component"
-import { FormContainer, PaymentFormContainer } from "./payment-form.styles"
+import { useState } from "react"
+import { useSelector } from "react-redux"
+import { selectCartTotal } from "../../store/cart/cart.selector"
+import { selectCurrentUser } from "../../store/user/user.selector"
+import { BUTTON_TYPE_CLASSES } from "../button/button.component"
+import { FormContainer, PaymentButton, PaymentFormContainer } from "./payment-form.styles"
 
 const BUTTON_TEXT = 'Pay Now'
 const FORM_TITLE = 'Card Payment'
@@ -8,18 +12,44 @@ const FORM_TITLE = 'Card Payment'
 export const PaymentForm = () => {
     const stripe = useStripe();
     const elements = useElements();
+    const amount = useSelector(selectCartTotal)
+    const currentUser = useSelector(selectCurrentUser)
+    const [isPaymentLoading, setIsPaymentLoading] = useState(false)
+
     const paymentHandler = async (e) => {
-        e.preventDefault();
+        setIsPaymentLoading(true);
+        e?.preventDefault();
         if (!stripe || !elements) return;
-        
+        const response = await fetch('/.netlify/functions/create-payment-intent', {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify( { amount: amount * 100 } )
+        }).then(res => res.json());
+
+    const { paymentIntent: { client_secret } } = response;
+    const paymentResult = await stripe.confirmCardPayment(client_secret, {
+        payment_method: {
+            card: elements.getElement(CardElement),
+            billing_details: {
+                name: currentUser ? currentUser.displayName : 'Guest'
+            }
+        }
+    })
+
+    setIsPaymentLoading(false);
+
+    if (paymentResult.error) console.log(paymentResult.error)
+    else if (paymentResult.paymentIntent.status === 'succeeded') console.log('Payment Succeeded')
     }
 
     return (
         <PaymentFormContainer>
-            <FormContainer>
+            <FormContainer onSubmit={paymentHandler}>
                 <h2>{FORM_TITLE}</h2>
                 <CardElement/>
-                <Button buttonType={BUTTON_TYPE_CLASSES.inverted}>{BUTTON_TEXT}</Button>
+                <PaymentButton isLoading={isPaymentLoading} buttonType={BUTTON_TYPE_CLASSES.inverted}>{BUTTON_TEXT}</PaymentButton>
             </FormContainer>
         </PaymentFormContainer>
     )
