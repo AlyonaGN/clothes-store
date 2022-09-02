3aec4236291a8c82fcc148ddc6b462baf9ad8cd7
Alena <a.gnevusheva@gmail.com>
2022-09-02 13:02:38 +0400

    close dropdown before navigating to next route, set prettier

--- src/components/cart-dropdown/cart-dropdown.component.tsx
+++ src/components/cart-dropdown/cart-dropdown.component.tsx
@@ -1,33 +1,33 @@
-import { useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import { selectCartItems } from '../../store/cart/cart.selector';
-import Button from '../button/button.component';
-import CartItem from '../cart-item/cart-item.component';
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { selectCartItems } from '../../store/cart/cart.selector'
+import { toggleIsOpen } from '../../store/cart/cartSlice'
+import { useAppDispatch } from '../../store/hooks'
+import Button from '../button/button.component'
+import CartItem from '../cart-item/cart-item.component'
 
-import {
-  CartDropdownContainer,
-  EmptyMessage,
-  CartItems,
-} from './cart-dropdown.styles';
+import { CartDropdownContainer, EmptyMessage, CartItems } from './cart-dropdown.styles'
 
 export const CartDropdown = () => {
-  const cartItems = useSelector(selectCartItems);
-  const navigate = useNavigate();
+    const dispatch = useAppDispatch()
+    const cartItems = useSelector(selectCartItems)
+    const navigate = useNavigate()
 
-  const goToCheckoutHandler = () => {
-    navigate('/checkout');
-  };
+    const goToCheckoutHandler = () => {
+        navigate('/checkout')
+        dispatch(toggleIsOpen())
+    }
 
-  return (
-    <CartDropdownContainer>
-      <CartItems>
-        {cartItems.length ? (
-          cartItems.map((item) => <CartItem key={item.id} cartItem={item} />)
-        ) : (
-          <EmptyMessage>Your cart is empty</EmptyMessage>
-        )}
-      </CartItems>
-      <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
-    </CartDropdownContainer>
-  );
-};
+    return (
+        <CartDropdownContainer>
+            <CartItems>
+                {cartItems.length ? (
+                    cartItems.map(item => <CartItem key={item.id} cartItem={item} />)
+                ) : (
+                    <EmptyMessage>Your cart is empty</EmptyMessage>
+                )}
+            </CartItems>
+            <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
+        </CartDropdownContainer>
+    )
+}
