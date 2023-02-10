f3a1964604dd3983cf50cb4bed7fffedbdffc7b9
Alena <a.gnevusheva@gmail.com>
2023-02-10 21:19:11 +0400

    refactor button component

--- src/components/button/button.component.tsx
+++ src/components/button/button.component.tsx
@@ -1,33 +1,31 @@
-import { ButtonHTMLAttributes, FC, PropsWithChildren } from 'react';
-import {
-  BaseButton,
-  ButtonSpinner,
-  GoogleSigninButton,
-  InvertedButton,
-} from './button.styles';
+import { ButtonHTMLAttributes, FC } from 'react'
+import { BaseButton, ButtonSpinner, GoogleSigninButton, InvertedButton } from './button.styles'
 
 export enum BUTTON_TYPE_CLASSES {
-  base = 'base',
-  google = 'google-sign-in',
-  inverted = 'inverted'
+    base = 'base',
+    google = 'google-sign-in',
+    inverted = 'inverted'
 }
 
 const getButton = (buttonType = BUTTON_TYPE_CLASSES.base) =>
-  ({
-    [BUTTON_TYPE_CLASSES.base]: BaseButton,
-    [BUTTON_TYPE_CLASSES.google]: GoogleSigninButton,
-    [BUTTON_TYPE_CLASSES.inverted]: InvertedButton,
-  }[buttonType])
+    ({
+        [BUTTON_TYPE_CLASSES.base]: BaseButton,
+        [BUTTON_TYPE_CLASSES.google]: GoogleSigninButton,
+        [BUTTON_TYPE_CLASSES.inverted]: InvertedButton
+    }[buttonType])
 
 export type ButtonProps = {
-    buttonType?: BUTTON_TYPE_CLASSES,
-    isLoading?: boolean,
-} & PropsWithChildren & ButtonHTMLAttributes<HTMLButtonElement>
-
+    buttonType?: BUTTON_TYPE_CLASSES
+    isLoading?: boolean
+} & ButtonHTMLAttributes<HTMLButtonElement>
 
 const Button: FC<ButtonProps> = ({ children, buttonType, isLoading, ...otherProps }) => {
-  const CustomButton = getButton(buttonType);
-  return <CustomButton disabled={isLoading} {...otherProps}>{isLoading ? <ButtonSpinner/> : children}</CustomButton>;
+    const CustomButton = getButton(buttonType)
+    return (
+        <CustomButton disabled={isLoading} {...otherProps}>
+            {isLoading ? <ButtonSpinner /> : children}
+        </CustomButton>
+    )
 }
 
-export default Button;
+export default Button
