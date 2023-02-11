b935e63f482b5a02985726338b2b5d2cb7cd1ffa
alyonagn <al.gnevusheva@gmail.com>
2023-02-11 12:11:08 +0400

    add apollo provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
 import './index.scss';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
@@ -12,8 +13,27 @@ import { stripePromise } from './utils/stripe/stripe.utils';
 const container = document.getElementById('root');
 const root = createRoot(container!);
 
+const client = new ApolloClient({
+  uri: 'http://localhost:4800/graphql',
+  cache: new InMemoryCache(),
+});
+
+client
+  .query({
+    query: gql`
+      query GetGategories {
+        categories {
+          id
+          title
+        }
+      }
+    `,
+  })
+  .then((result) => console.log(result));
+
 root.render(
   <React.StrictMode>
+    <ApolloProvider client={client}>
     <Provider store={store}>
       <PersistGate persistor={persistor}>
       <BrowserRouter>
@@ -23,5 +43,6 @@ root.render(
       </BrowserRouter>
       </PersistGate>
     </Provider>
+    </ApolloProvider>
   </React.StrictMode>
 );
